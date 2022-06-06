7715a3af2a08e000c303a0c404cf9fba189a886b
Tal <benhatal@post.bgu.ac.il>
2022-06-06 18:34:38 +0300

    sec commit

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -18,8 +18,17 @@ async function getRecipeInformation(recipe_id) {
     });
 }
 
+async function getRecipesPreview(recipes_ids_list){
+    let promises = [];
+    recipes_ids_list.map((id) => {
+        promises.push(getRecipeInformation(id));
+    });
+    let info_res = await Promise.all(promises);
+    return extractPreviewRecipeDetails(info_res);
+}
 
 
+// preview information
 async function getRecipeDetails(recipe_id) {
     let recipe_info = await getRecipeInformation(recipe_id);
     let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipe_info.data;
@@ -37,9 +46,63 @@ async function getRecipeDetails(recipe_id) {
     }
 }
 
+/**
+ * extract the specifics details for preview recipe
+ * @param {*} recipes_info 
+ * @returns list of recipes with preview data
+ */
+function extractPreviewRecipeDetails(recipes_info){
 
+    return recipes_info.map((recipe_info) => {
+
+        let data = recipe_info;
+        if (recipe_info.data){
+            data = recipe_info.data;
+        }
+        const {
+            id,
+            title,
+            readyInMinutes,
+            image,
+            aggregateLikes,
+            vegan,
+            vegetarian,
+            glutenFree,
+        } = data;
+        return {
+            id: id,
+            title: title,
+            image: image,
+            readyInMinutes: readyInMinutes,
+            popularity: aggregateLikes,
+            vegan: vegan,
+            vegetarian: vegetarian,
+            glutenFree: glutenFree
+        }
+    });
+}
+
+
+async function getRandomRecipes(){
+    const response = await axios.get(`${api_domain}/random`, {
+        params: {
+            number: 10,
+            apiKey: process.env.spooncular_apiKey
+        }
+    });
+    return response;
+}
+
+async function getRandomThreeRecipes(){
+    let random_pool = await getRandomRecipes();
+    let filtered_random_pool = random_pool.data.recipes.filter((random) => (random.instructions != "") && (random.image && random.title))
+    if (filtered_random_pool < 3)
+        return getRandomThreeRecipes();
+    return extractPreviewRecipeDetails([filtered_random_pool[0],filtered_random_pool[1],filtered_random_pool[2]])
+}
 
 exports.getRecipeDetails = getRecipeDetails;
-
+exports.getRandomThreeRecipes = getRandomThreeRecipes;
+exports.getRecipesPreview = getRecipesPreview;
 
 
