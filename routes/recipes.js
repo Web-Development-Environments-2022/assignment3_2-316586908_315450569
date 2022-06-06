88995adf473f3e922062809c7a33ba73bc6c39a4
Tal <benhatal@post.bgu.ac.il>
2022-06-06 18:34:58 +0300

    sec push

--- routes/recipes.js
+++ routes/recipes.js
@@ -17,4 +17,30 @@ router.get("/:recipeId", async (req, res, next) => {
   }
 });
 
+/**
+ * This path returns 3 random recipes
+ */
+router.get("/random", async (req, res, next) => {
+  try{
+    let random_3_recipes = await recipes_utils.getRandomThreeRecipes();
+    res.send(random_3_recipes);
+  }
+  catch (error){
+    next(error);
+  }
+});
+
+/**
+ * list of preview recipes, for example : search recipe page
+ * TODO: function to search recipe. question 8 . need use spooncular API
+ */
+ router.get("/:query", async (req, res, next) => {
+  try {
+    const recipesPreviews = await recipes_utils.getRecipesPreview(req.params.number);
+    res.send(recipesPreviews);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
